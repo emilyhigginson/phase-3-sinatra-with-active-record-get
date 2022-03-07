094dbaf05c05742aba08bd0f2c4d2651777e2e78
Emily <emilyhigginson14@gmail.com>
2022-03-07 07:29:40 -0500

    commit tree bcefc35c7b3a812c5f5e97b276d8d72e22bb9879

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+    # get all the games from the database
+    # return a JSON response with an array of all the game data
+  end
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    game.to_json(include: { reviews: { include: :user } })
+
+    # send a JSON-formatted response of the game data
+
   end
 
 end
